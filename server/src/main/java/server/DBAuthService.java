c0d9f77cdbc9ef0e932830b81831f53a4607382d
Kirill <kiruxin16@gmail.com>
2022-03-23 23:30:14 +0700

    ДЗ. В базе данных столбцы login и nickname  - уникальные

---
+++ server/src/main/java/server/DBAuthService.java
@@ -0,0 +1,57 @@
+package server;
+
+import java.sql.SQLException;
+
+public class DBAuthService implements AuthService {
+
+    private DBHandler dbHandler;
+
+
+    public DBAuthService() {
+        dbHandler = new DBHandler();
+
+    }
+
+    @Override
+    public String getNicknameByLoginAndPassword(String login, String password) {
+        String nickname=null; //мне показалось, что, если ввести строковую переменную код будет более очевиден
+        try {
+            dbHandler.connect();
+            nickname = dbHandler.getNickname(login,password);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            dbHandler.disconnect();
+            return nickname;
+
+        }
+
+
+    }
+
+    @Override
+    public boolean isNameExist(String name) {
+        try {
+            return dbHandler.isUserExist(name);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean registration(String login, String password, String nickname) {
+        try {
+            dbHandler.connect();
+            dbHandler.addUser(login,password,nickname);
+            return true;
+        }catch (Exception e){
+            return false;
+        }finally {
+            System.out.println("its alive!!");
+            dbHandler.disconnect();
+        }
+
+    }
+}
---
+++ server/src/main/java/server/DBHandler.java
@@ -0,0 +1,77 @@
+package server;
+
+import java.sql.*;
+
+
+public class DBHandler {
+    private Connection connection;
+    private Statement statement;
+    private PreparedStatement psInsert;
+
+    public DBHandler() {
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void connect() throws Exception{
+
+        connection=DriverManager.getConnection("jdbc:sqlite:users.db");
+        statement=connection.createStatement();
+    }
+
+    public boolean isUserExist(String name) throws SQLException{
+
+        ResultSet rs=statement.executeQuery("SELECT FROM users WHERE login="+name+";");
+        if(rs.next()){
+            return true;
+        }
+        return false;
+    }
+
+    public String getNickname(String login, String password) throws SQLException{
+        psInsert = connection.prepareStatement("SELECT nickname FROM users WHERE login=? AND password=?;");
+        psInsert.setString(1,login);
+        psInsert.setString(2,password);
+        String result =null;
+        ResultSet rs=psInsert.executeQuery();
+        if (rs.next()){
+            result=rs.getString(1);
+        }
+        return result;
+    }
+
+    public void addUser(String login, String password, String nickname) throws SQLException{
+        psInsert = connection.prepareStatement("INSERT INTO users (login,password,nickname) VALUES (?,?,?)");
+        psInsert.setString(1,login);
+        psInsert.setString(2,password);
+        psInsert.setString(3,nickname);
+        psInsert.executeUpdate();
+
+    }
+
+
+
+    public  void disconnect(){
+        try {
+
+            statement.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        try {
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+
+
+    
+
+
+}
